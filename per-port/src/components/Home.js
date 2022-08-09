e376c96a60514e00701ce8ac36c959a30bf88829
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-08-09 18:46:04 +0100

    home section incomplete

--- per-port/src/components/Home.js
+++ per-port/src/components/Home.js
@@ -1,29 +1,42 @@
 import React from "react";
-//import HeroImage from "../assets/portfolio/heroImage.jpg";
+import "./home.css";
+import HeroImage from "../assets/portfolio/heroImage.jpg";
 import { TbArrowRightCircle } from "react-icons/tb";
 
 const Home = () => {
     return (
         <div
+            id="body"
             name="home"
-            className="h-screen w-full bg-gradient-b from-black to-gray-800 "
+            className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
         >
-            <div>
+            <div
+                className="max-w-screen-lg mx-auto flex flex-col
+                              items-center justify-center h-full px-4 md:flex-row "
+            >
                 <div>
                     <h2>I am a Front-End Developer</h2>
-                    <p>
+                    <p id="bio">
                         {" "}
                         Add a brief bio of experience and prefrence in techs and
                         my evolution as a developer
                     </p>
                     <div>
-                        <button>
+                        <button id="portfolio-btn">
                             Portfolio{" "}
                             <span>
                                 <TbArrowRightCircle />
                             </span>
                         </button>
                     </div>
+                </div>
+                <div>
+                    <img
+                        id="profile-image"
+                        src={HeroImage}
+                        alt="hero"
+                        className="rounded-2xl mx-auto w-2/3 md:w-full"
+                    />
                 </div>
             </div>
         </div>
