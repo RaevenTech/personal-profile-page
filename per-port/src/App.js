9264ed7467114fa0c9816ea6cab6c7ff33405a5a
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-08-09 15:47:22 +0100

    setup done and navbar 80%

--- per-port/src/App.js
+++ per-port/src/App.js
@@ -1,12 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>
+        <div>
+            <Navbar />
         </div>
     );
 }
