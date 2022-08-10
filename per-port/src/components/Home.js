3c8d0ea9aeee44826a9395e500ca6a3406db058b
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-08-10 16:12:53 +0100

    hero home

--- per-port/src/components/Home.js
+++ per-port/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home.css";
-import HeroImage from "../assets/portfolio/heroImage.jpg";
+import HeroImage from "../assets/portfolio/profilePic.jpg";
 import { TbArrowRightCircle } from "react-icons/tb";
 
 const Home = () => {
@@ -8,18 +8,22 @@ const Home = () => {
         <div
             id="body"
             name="home"
-            className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 text-white"
+            className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 "
         >
             <div
                 className="max-w-screen-lg mx-auto flex flex-col
                               items-center justify-center h-full px-4 md:flex-row "
             >
-                <div className="flex flex-col justify-center h-full">
-                    <h2>I am a Front-End Developer</h2>
-                    <p id="bio">
+                <div className="flex flex-col justify-center h-content">
+                    <h2 className="text-4xl sm:text-7xl font-bold  text-white">
+                        I am a Front-End Developer
+                    </h2>
+                    <p id="bio" className="text-gray-500 py-4 max-w-md">
                         {" "}
                         Add a brief bio of experience and prefrence in techs and
-                        my evolution as a developer
+                        my evolution as a developer. Using technologies like
+                        HTML, CSS, JavaScript, React, React Router DOM,
+                        Tailwind, bootstrap and Firebase
                     </p>
                     <div>
                         <button id="portfolio-btn">
