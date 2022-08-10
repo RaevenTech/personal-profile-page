82a2f6bc93b2eb87abf5e3057f0eb04e96cbd82e
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-08-10 15:30:05 +0100

    hero home

--- per-port/src/components/Home.js
+++ per-port/src/components/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
         <div
             id="body"
             name="home"
-            className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+            className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 text-white"
         >
             <div
                 className="max-w-screen-lg mx-auto flex flex-col
                               items-center justify-center h-full px-4 md:flex-row "
             >
-                <div>
+                <div className="flex flex-col justify-center h-full">
                     <h2>I am a Front-End Developer</h2>
                     <p id="bio">
                         {" "}
