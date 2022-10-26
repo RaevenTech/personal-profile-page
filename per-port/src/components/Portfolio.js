d613321aec5d381847831ee23a9709e520c50624
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-10-26 11:40:24 +0100

    project added to portfolio page

--- per-port/src/components/Portfolio.js
+++ per-port/src/components/Portfolio.js
@@ -1,22 +1,34 @@
+//import { ExternalLink } from "react-external-link";
 import arrayDestruct from "../assets/portfolio/projects1.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
+//import navbar from "../assets/portfolio/navbar.jpg";
 import reactWeather from "../assets/portfolio/projects3.jpg";
+import javascript from "../assets/javaScript-bg.jpg";
 
 const Portfolio = () => {
     const projects = [
         {
             id: 1,
             src: arrayDestruct,
+            description: " Coming soon",
+            url: "Coming soon",
+            code: "",
         },
 
         {
             id: 2,
-            src: navbar,
+            src: javascript,
+            description:
+                "Collection of small project using JavaScript,HTML and CSS",
+            url: "https://javascript-simple.vercel.app/",
+            code: "https://github.com/RaevenTech/javascript-simple",
         },
 
         {
             id: 3,
             src: reactWeather,
+            description: "Coming soon",
+            url: "Coming soon",
+            code: "",
         },
     ];
 
@@ -35,11 +47,9 @@ const Portfolio = () => {
                     </p>
                     <p className="py-6">Check out some of my work here</p>
                 </div>
-                <div
-                    className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 pr-4
-                     sm:px-0 md:px-16"
-                >
-                    {projects.map(({ id, src }) => (
+                {/*-- mapping through the projects to be displayed on the screen */}
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 pr-4 sm:px-0 md:px-16">
+                    {projects.map(({ id, src, code, url, description }) => (
                         <div
                             key={id}
                             className="shadow-md shadow-gray-600 rounded-lg"
@@ -49,13 +59,16 @@ const Portfolio = () => {
                                 alt=""
                                 className="rounded-md duration-200 hover:scale-105"
                             />
+                            <p className=" text-gray-500 text-md mt-3">
+                                {description}
+                            </p>
                             <div className="flex items-center justify-center">
                                 <button className="w-1/2  text-orange-600 px-6 py-1 m-4 duration-200 hover:scale-105">
-                                    Demo projects
+                                    <a href={code}> Code</a>
                                 </button>
 
                                 <button className="w-1/2 px-6 text-orange-600  py-1 m-4 duration-200 hover:scale-105">
-                                    coming soon
+                                    <a href={url}> View</a>
                                 </button>
                             </div>
                         </div>
